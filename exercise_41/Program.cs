a9fb8593847a9624e2c51743ebe1648ac564edb2
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-15 20:31:54 +0300

    exercsise 41 had been accomplished

---
+++ exercise_41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Enter the length of array you want to fill: ");
+
+int[] Array = new int[Convert.ToInt32(Console.ReadLine())];
+
+for (int i = 0, sum = 0; i <= Array.Length; i++)
+{
+    if (i == Array.Length)
+    {
+        Console.WriteLine($"The number of the values greater than zero is {sum}"); 
+        break;
+    }
+    Console.Write($"Enter a value of {i} element: "); 
+    Array[i] = Convert.ToInt32(Console.ReadLine());
+    if (Array[i]>0) sum += 1;
+}
